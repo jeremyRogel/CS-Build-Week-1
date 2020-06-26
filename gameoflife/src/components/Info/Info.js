d6458c8e159f3ac490dea440f985afa720d49ac8
Jeremy Rogel <jeremyrogel@Jeremys-MacBook-Pro.local>
2020-06-26 16:39:41 -0400

    updated info section

--- gameoflife/src/components/Info/Info.js
+++ gameoflife/src/components/Info/Info.js
@@ -3,11 +3,10 @@ import React from 'react'
 export default function Info() {
     return (
         <>
-        <h3>INFO</h3>
-        <p>This is the info section</p>
-
-        <p>This is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info sectionThis is the info section</p>
-
+        <h3>Rules of the Game</h3>
+        <p> Any cell with fewer than two live neighbors dies (as if by isolation)
+Any cell with two or three living neighbors survives to the next generation
+Any cell with four or more neighbors dies (as if by over population) </p>
         </>
     )
 }
