404e76fff68ce3d2296c04cb9722827389ac63b3
Jeremy Rogel <jeremyrogel@Jeremys-MacBook-Pro.local>
2020-06-26 16:42:29 -0400

    updated html in info.js

--- gameoflife/src/components/Info/Info.js
+++ gameoflife/src/components/Info/Info.js
@@ -4,9 +4,12 @@ export default function Info() {
     return (
         <>
         <h3>Rules of the Game</h3>
-        <p> Any cell with fewer than two live neighbors dies (as if by isolation)
-Any cell with two or three living neighbors survives to the next generation
-Any cell with four or more neighbors dies (as if by over population) </p>
+        <h4>1)</h4>
+            <p> Any cell with fewer than two live neighbors dies (as if by isolation)</p>
+        <h4>2)</h4>
+            <p>Any cell with two or three living neighbors survives to the next generation</p>
+        <h4>3)</h4>
+            <p>Any cell with four or more neighbors dies (as if by over population)</p>
         </>
     )
 }
